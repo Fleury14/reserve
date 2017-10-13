d5a217152bd24f1b65983ac1423174ce264cf88b
J.R. Ruggiero <fleury14@comcast.net>
2017-10-12 21:04:20 -0700

    class work from 10/12

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 // import { RouterModule, Routes } from '@angular/router';
 
 import { RootComponent } from './app-component';
 import { NavComponent } from './nav/nav-component';
 import { AppRoutingModule, routingComponents } from './app.routing.module';
 import { LoginComponent } from './login/login.component';
+import { RoomFormComponent} from './room/room-form/room-form.component';
 
 import { LoginService } from './services/login.service';
 // import { WhateverComponent } from './whatever/whatever-component';
@@ -20,8 +22,8 @@ import { LoginService } from './services/login.service';
 // }];
 
 @NgModule({
-    declarations: [ RootComponent, NavComponent, routingComponents, LoginComponent],
-    imports: [ BrowserModule, AppRoutingModule ],
+    declarations: [ RootComponent, NavComponent, routingComponents, LoginComponent, RoomFormComponent],
+    imports: [ BrowserModule, AppRoutingModule, FormsModule ],
     providers: [ LoginService ],
     bootstrap: [ RootComponent ]
 })
