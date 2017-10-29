37fa12d9a1c46caf5e4c1b4ad9f5ba91ac0a6465
J.R. Ruggiero <fleury14@comcast.net>
2017-10-29 10:10:47 -0700

    commented room list component

--- src/app/room/room-list/room-list.component.ts
+++ src/app/room/room-list/room-list.component.ts
@@ -15,27 +15,33 @@ import { RoomService } from './../../services/room.service';
 
 export class RoomListComponent implements OnInit {
 
-
-    // public roomId: string;
-
+    // declare room that will hold our list. note that its typed to maintain intergrity
     public room: IRoom;
 
-
+    // inject necessary services.. router and room. router will be used to get the current room
+    // and room will be used to get the contents, and delete when necessary
     constructor( private _route: ActivatedRoute, private _roomService: RoomService) { }
 
     public ngOnInit() {
 
-
+        // subscribe to the route using the PARENT parammap becuase the current param is list/form
+        // then call the switch room function get get the reservations for that room. also note that
+        // this is done oninit instead of in the constructor. furthermore, because its suscribed to the
+        // route which then sunbscribes the room instead of just subscribing to the room, if the user clicks on a
+        // different room, the list will adjust accordingly... which is pretty damn neat.
         this._route.parent.paramMap.subscribe(route => {
             this._switchRoom(route.get('id'));
         });
 
     }
 
+    // the method to get the information for the current room. because its referred to in oninit it runs at least once, and
+    // because its subscribed to the route, it alsoo is run when the route param changes
     private _switchRoom(id: string) {
         this._roomService.getRoomById(id).subscribe(room => { this.room = room; });
     }
 
+    // simple method to call the deletion method inside the room service when necessary
     public deleteReservation(id) {
         this._roomService.deleteReservation(this.room.id, id);
     }
