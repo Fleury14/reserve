6a95d4104453da228ff5889f8ebf2ee17cf3aafc
J.R. Ruggiero <fleury14@comcast.net>
2017-11-01 12:55:23 -0700

    modify reservation interface to prepare for animation on room list

--- src/interfaces/reservation.interface.ts
+++ src/interfaces/reservation.interface.ts
@@ -6,6 +6,10 @@ interface IReservation {
     endTime: Date;
     emailConfirmation: string;
     isAgreed: string;
+    [propName: string]: any;
+
+    // the propname line allows any property to be added in addition to the ones already required. we'll be using
+    // this for an animation flag.
 }
 
 export default IReservation;
